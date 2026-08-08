2d36a02914863f14ac900bea4163e44a9df44eb1
Ricardo Woo <wooosmar@gmail.com>
2026-08-08 13:29:27 -0500

    Refactor get_brain_inputs to use food_grid for improved efficiency

--- organism.py
+++ organism.py
@@ -117,15 +117,16 @@ class Organism:
 
     # SENSORS
 
-    def get_brain_inputs(self, foods):
+    def get_brain_inputs(self, food_grid):
         closest_food = None
         closest_distance = self.vision
 
-        for food in foods:
+        nearby_food = food_grid.query(self.x, self.y, self.vision)
 
-            distance = math.sqrt((self.x - food.x) ** 2 + (self.y - food.y) ** 2)
+        for food in nearby_food:
+            distance = math.hypot(self.x - food.x, self.y - food.y)
 
-            if distance < closest_distance and distance <= self.vision:
+            if distance < closest_distance:
                 closest_distance = distance
                 closest_food = food
 
