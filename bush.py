cf1a869aea2d1f7ba7e9cf777992fd33de9e0d0d
Ricardo Woo <wooosmar@gmail.com>
2026-08-09 16:14:40 -0500

    Refactor draw method in Bush class: adjust scale calculation and improve code formatting

--- bush.py
+++ bush.py
@@ -12,9 +12,15 @@ class Bush:
     def draw(self, screen, camera):
         screen_x, screen_y = camera.world_to_screen(self.x, self.y)
 
-        scale = camera.zoom * 3
+        scale = camera.zoom
 
-        image = pygame.transform.scale(self.image,(int(self.image.get_width() * scale),int(self.image.get_height() * scale),))
+        image = pygame.transform.scale(
+            self.image,
+            (
+                int(self.image.get_width() * scale),
+                int(self.image.get_height() * scale),
+            ),
+        )
 
         rect = image.get_rect(center=(screen_x, screen_y))
         screen.blit(image, rect)
