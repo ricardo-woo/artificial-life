5ffbb132d71a01cb035b7374a03cc2d037a7ae8f
Ricardo Woo <wooosmar@gmail.com>
2026-08-09 16:59:17 -0500

    Refactor SpatialGrid class: improve code readability and formatting

--- spatialgrid.py
+++ spatialgrid.py
@@ -1,26 +1,31 @@
 import math
 
+
 class SpatialGrid:
     def __init__(self, cell_size):
         self.cell_size = cell_size
         self.cells = {}
+        self.object_cells = {}
 
     def clear(self):
         self.cells.clear()
 
-    def get_cell(self,x,y):
-        return(int(x // self.cell_size), int(y // self.cell_size))
+    def get_cell(self, x, y):
+        return (int(x // self.cell_size), int(y // self.cell_size))
 
     def insert(self, target, x, y):
-        cell = self.get_cell(x,y)
+        cell = self.get_cell(x, y)
 
         if cell not in self.cells:
             self.cells[cell] = []
 
         self.cells[cell].append(target)
 
-    def remove(self, target,x,y):
-        cell = self.get_cell(x,y)
+    def remove(self, target):
+        cell = self.object_cells.get(target)
+
+        if cell is None:
+            return
 
         if cell in self.cells:
             self.cells[cell].remove(target)
@@ -28,12 +33,24 @@ class SpatialGrid:
             if not self.cells[cell]:
                 del self.cells[cell]
 
-    def query(self, x, y, radius):
-        min_cell_x = int((x-radius) // self.cell_size)
-        max_cell_x = int((x+radius) // self.cell_size)
+        del self.object_cells[target]
 
-        min_cell_y = int((y-radius) // self.cell_size)
-        max_cell_y = int((y+radius) // self.cell_size)
+    def update(self, target, x, y):
+        old_cell = self.object_cells.get(target)
+        new_cell = self.get_cell(x, y)
+
+        if old_cell == new_cell:
+            return
+
+        self.remove(target)
+        self.insert(target, x, y)
+
+    def query(self, x, y, radius):
+        min_cell_x = int((x - radius) // self.cell_size)
+        max_cell_x = int((x + radius) // self.cell_size)
+
+        min_cell_y = int((y - radius) // self.cell_size)
+        max_cell_y = int((y + radius) // self.cell_size)
 
         results = []
 
