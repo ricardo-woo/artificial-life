9f0f723d702de387176892a96063ed1cce18422d
Ricardo Woo <wooosmar@gmail.com>
2026-08-09 15:40:14 -0500

    Refactor code formatting: improve readability by adjusting whitespace and line breaks

--- main.py
+++ main.py
@@ -13,10 +13,27 @@ from Population import Population
 from SaveManager import SaveManager
 from spatialgrid import SpatialGrid
 from settings import (
-    BACKGROUND_COLOR, FAST_FORWARD_SPEED, FOOD_COUNT, FOOD_ENERGY_VAL, SPATIAL_CELL_SIZE,
-    FOOD_RESPAWN_INTERVAL, FPS, GENERATION_END_WAIT_TIME, HEIGHT, KEY_FAST_FORWARD,
-    KEY_FOLLOW_ORGANISM, KEY_NEXT_GENERATION, KEY_PAUSE_SELECTION, MAX_ENERGY, BUSH_COUNT,
-    SAVE_INTERVAL_MS, WIDTH, WORLD_HEIGHT, WORLD_WIDTH, SAVE_FILE_PATH, KEY_DEBUG
+    BACKGROUND_COLOR,
+    FAST_FORWARD_SPEED,
+    FOOD_COUNT,
+    FOOD_ENERGY_VAL,
+    SPATIAL_CELL_SIZE,
+    FOOD_RESPAWN_INTERVAL,
+    FPS,
+    GENERATION_END_WAIT_TIME,
+    HEIGHT,
+    KEY_FAST_FORWARD,
+    KEY_FOLLOW_ORGANISM,
+    KEY_NEXT_GENERATION,
+    KEY_PAUSE_SELECTION,
+    MAX_ENERGY,
+    BUSH_COUNT,
+    SAVE_INTERVAL_MS,
+    WIDTH,
+    WORLD_HEIGHT,
+    WORLD_WIDTH,
+    SAVE_FILE_PATH,
+    KEY_DEBUG,
 )
 from Simulation.SimulationClock import SimulationClock
 from ui import UIManager
@@ -109,7 +126,6 @@ while running:
                         foods.append(new_food)
                         food_grid.insert(new_food, new_food.x, new_food.y)
 
-
                     waiting_for_next_gen = False
                     generation_simulation_time = 0
                     food_respawn_timer = 0
@@ -160,7 +176,7 @@ while running:
             food_grid.clear()
             for _ in range(FOOD_COUNT):
                 new_food = Food(bushes)
-                foods.append( new_food)
+                foods.append(new_food)
                 food_grid.insert(new_food, new_food.x, new_food.y)
             waiting_for_next_gen = False
             generation_simulation_time = 0
@@ -179,7 +195,7 @@ while running:
 
             organism.update(food_grid, dt)
 
-            nearby_food = food_grid.query( organism.x, organism.y, organism.vision)
+            nearby_food = food_grid.query(organism.x, organism.y, organism.vision)
 
             for food in nearby_food:
 
@@ -188,9 +204,7 @@ while running:
 
                 if organism.eat(food):
                     organism.food_eaten += 1
-                    organism.energy = min(
-                        MAX_ENERGY, organism.energy + FOOD_ENERGY_VAL
-                    )
+                    organism.energy = min(MAX_ENERGY, organism.energy + FOOD_ENERGY_VAL)
                     organism.time_since_food = 0
                     foods.remove(food)
                     food_grid.remove(food, food.x, food.y)
@@ -198,9 +212,9 @@ while running:
 
         food_respawn_timer += dt
         while food_respawn_timer >= FOOD_RESPAWN_INTERVAL and len(foods) < FOOD_COUNT:
-            new_food=Food(bushes)
+            new_food = Food(bushes)
             foods.append(new_food)
-            food_grid.insert(new_food, new_food.x,new_food.y)
+            food_grid.insert(new_food, new_food.x, new_food.y)
             food_respawn_timer -= FOOD_RESPAWN_INTERVAL
 
         if selected_organism is not None:
@@ -245,8 +259,12 @@ while running:
 
     if debug_open:
         leaderboard_rows = ui_manager.draw_debug(
-            screen, organisms, foods, generation, generation_simulation_time,
-            clock.get_fps()
+            screen,
+            organisms,
+            foods,
+            generation,
+            generation_simulation_time,
+            clock.get_fps(),
         )
 
     pygame.display.flip()
