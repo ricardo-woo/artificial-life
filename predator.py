da483e76704c4e809bb19d90a08a7c9806dfe8f3
Ricardo Woo <wooosmar@gmail.com>
2026-08-10 14:14:47 -0500

    Add Predator class with energy management and kill tracking

---
+++ predator.py
@@ -0,0 +1,22 @@
+from organism import Organism
+
+from settings import MAX_ENERGY
+
+
+class Predator(Organism):
+    def __init__(self, x, y, genome):
+        super().__init__(x, y, genome)
+
+        self.type = "predator"
+
+        self.kills = 0
+
+    def update(self, food_grid, organism_grid, dt):
+        return super().update(food_grid, organism_grid, dt)
+
+    def eat_organism(self, target):
+        if self.eat(target):
+            self.energy = min(MAX_ENERGY, self.energy + target.energy * 0.5)
+            self.kills += 1
+            return True
+        return False
