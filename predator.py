7212462969b947fa072fdad7fd44ba0c87f58da5
Ricardo Woo <wooosmar@gmail.com>
2026-08-10 16:45:16 -0500

    Refactor Predator class to improve prey catching logic and streamline energy consumption

--- predator.py
+++ predator.py
@@ -1,4 +1,5 @@
 from organism import Organism
+import math
 
 from settings import MAX_ENERGY, PREDATOR_SPRITE, RAY_FOV_PREDATORS
 
@@ -16,10 +17,22 @@ class Predator(Organism):
     def update(self, food_grid, organism_grid, dt):
         return super().update(food_grid, organism_grid, dt)
 
+    def can_catch(self, prey):
+        dx = prey.x - self.x
+        dy = prey.y - self.y
+
+        angle_to_prey = math.atan2(dy, dx)
+
+        predator_alignment = math.cos(self.angle - angle_to_prey)
+
+        prey_alignment = math.cos(prey.angle - angle_to_prey)
+
+        return predator_alignment > 0.5 and prey_alignment > 0.3 and self.eat(prey)
+
     def eat_organism(self, target):
-        if self.eat(target):
-            self.energy = min(MAX_ENERGY, self.energy + target.energy)
-            self.food_eaten += 1
-            target.energy = 0
-            return True
-        return False
+        if not self.can_catch(target):
+            return False
+
+        self.energy = min(MAX_ENERGY, self.energy + target.energy)
+        target.energy = 0
+        return True
